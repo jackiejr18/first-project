cd2aee34d992ae61b250704f987adce3cb2da63e
Jackie Jr <jwysinger@gmail.com>
2023-11-19 12:44:11 -0600

    array that displays all users in h2 element

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 const App = () => {
   let x = 'JackieJr18';
+  let array = ['user1', 'user2', 'user3', 'user4', 'user5'];
   return (
     <div>
       {x}
+      {array.map((user)=>{return <h2>{user}</h2>})}
     </div>
   )
 }
