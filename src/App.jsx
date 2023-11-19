49dd2dab3244e2b3599510b3dd06a49f3a36ff10
Jackie Jr <jwysinger@gmail.com>
2023-11-19 17:57:19 -0600

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 const App = () => {
-  let x = 'JackieJr18';
-  let array = ['user1', 'user2', 'user3', 'user4', 'user5'];
+  let data = 'boy';
+
   return (
     <div>
-      {x}
-      {array.map((user)=>{return <h2>{user}</h2>})}
+      {data==='boy'?<h1>Boy</h1>: <h1>Girl</h1>}
     </div>
   )
 }
