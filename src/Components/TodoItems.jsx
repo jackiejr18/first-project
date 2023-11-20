d96d321151544c639baaedeed750b353fee6d0f8
Jackie Jr <jwysinger@gmail.com>
2023-11-20 17:16:30 -0600

    Images added to assets folder, and rendered in the app whne typed and added

--- src/Components/TodoItems.jsx
+++ src/Components/TodoItems.jsx
@@ -1,9 +1,17 @@
 import './CSS/TodoItems.css'
+import tick from './Assets/tick.png';
+import not_tick from './Assets/not_tick.png';
+import cross from './Assets/cross.png';
 
-const TodoItems = () => {
+const TodoItems = ({no,display,text}) => {
   return (
-    <div>
-      
+    <div className='todoitems'>
+      <div className='todoitems-container'>
+        <img src={not_tick}/>
+        <img src={tick}/>
+        <div className='todoitems-text'>{text}</div>
+      </div>
+      <img src={cross}/>
     </div>
   )
 }
